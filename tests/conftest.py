d5bc9f60a72e2fbaec89c73344c28a08ebead060
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-03-14 21:05:55 +0300

    add first tests

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from selene import browser
+from resources.resolutions import *
+
+
+@pytest.fixture(params=[FULL_HD, HD])
+def desktop_browser(request):
+    browser.config.base_url = 'https://github.com'
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture(params=[iPHONE_14_PRO_MAX, iPHONE_12_PRO])
+def mobile_browser(request):
+    browser.config.base_url = 'https://github.com'
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
+
+    yield
+
+    browser.quit()
